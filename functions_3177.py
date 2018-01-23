a0871fb0eaa155958b51a8d25140d2cbce57eb9a
Unknown <zhy.david@gmail.com>
2018-01-23 21:06:11 +0800

    Split program to 2 source files

---
+++ functions_3177.py
@@ -0,0 +1,129 @@
+from os import path, makedirs
+from shutil import rmtree
+from heapq import nlargest, nsmallest
+'''
+Author: David Zhu (P1703177)
+Class: DISM/FT/1A/21
+'''
+
+''' takes in a single split line returns the sales value of the line
+returns none if no valid float value is found or line format is wrong'''
+def lineValidation(splitline):
+    if len(splitline) != 6: # skip line if it has invalid format
+        return None
+    try:
+        return float(splitline[4])
+    except ValueError: # skip line if it has invalid data
+        return None
+
+'''reference: https://stackoverflow.com/questions/273192/how-can-i-create-a-directory-if-it-does-not-exist
+https://stackoverflow.com/questions/6996603/how-to-delete-a-file-or-folder
+Takes in file pointer as parameter
+Writes indivial city purchase records out to disk'''
+def writeRecords(fp):
+    cityFiles = {}
+    folder = 'reports'
+    if path.exists(folder):
+        rmtree(folder) # empty and remove directory if directory already exisits
+    makedirs(folder) # create reports directory
+    fpReadline = fp.readline
+    line = fpReadline()
+    while line:
+        tmp = line.split("\t")
+        if lineValidation(tmp) == None: # skip line if it has invalid format
+            line = fpReadline()
+            continue
+        city = tmp[2]
+        tmp = cityFiles.get(city) # try to get filepointer for city
+        if tmp == None: # if filepointer is not yet opened and stored
+            tmp = open('{}/{}.txt'.format(folder, city), 'w')
+            tmp.write(line)
+            cityFiles[city] = tmp
+        else:
+            tmp.write(line)
+        line = fpReadline()
+    for f in cityFiles.values(): # close file pointers
+        f.close()
+
+'''reference: https://www.w3resource.com/python-exercises/dictionary/python-data-type-dictionary-exercise-22.php
+takes in a dictonary and n as parameter where n is number of top values
+returns top n keys with values from dict'''
+def getTop(dictionary, n):
+    top = nlargest(n, dictionary, key=dictionary.get) # get n keys with highest values
+    for i in range(n):
+        top[i] = '{:<23}{:23.2f}'.format(top[i], dictionary[top[i]])
+    return top
+
+''' takes in a dictonary and n as parameter where n is number of lowest values
+returns bottom n keys with values from dict '''
+def getBottom(dictionary, n):
+    btm = nsmallest(n, dictionary, key=dictionary.get) # get n keys with lowest values
+    btm.reverse()
+    for i in range(n):
+        btm[i] = '{:<23}{:23.2f}'.format(btm[i], dictionary[btm[i]])
+    return btm
+
+''' takes in citySales, category sales and total sales amt as parameters
+prints out the sales summary for all cities and categories '''
+def printSummary(citySales, catSales, totalSale):
+    noCities = len(citySales)
+    noCats = len(catSales)
+    if noCities == 0 or noCats == 0:
+        print("Invalid data file. Operation aborted.")
+        return
+    avgCitySale = totalSale / noCities
+    avgCatSale = totalSale / noCats
+    divider = '=' * 46
+    print("Total Sales of the year is {:.2f}\n".format(totalSale))
+    print("The Average Sales From {} Cities:\n{:46.2f}\n".format(noCities, avgCitySale))
+    if noCities > 3:
+        print("Top Three Cities\n{}".format(divider))
+        for city in getTop(citySales, 3):
+            print(city)
+        print("{0}\n\nBottom Three Cities\n{0}".format(divider))
+        for city in getBottom(citySales, 3):
+            print(city)
+    else: # different format for 3 or less cities
+        print("Sales Figures by Cities\n{}".format(divider))
+        for city in getTop(citySales, noCities):
+            print(city)
+    print("{}\n\nThe Average Sales From {} Item Categories:\n{:46.2f}\n".format(divider, noCats, avgCatSale))
+    if noCats > 3:
+        print("Top Three Item Categories\n{}".format(divider))
+        for cat in getTop(catSales, 3):
+            print(cat)
+        print("{0}\n\nBottom Three Item Categories\n{0}".format(divider))
+        for cat in getBottom(catSales, 3):
+            print(cat)
+    else: # different format for 3 or less categories
+        print("Sales Figures by Item Categories\n{}".format(divider))
+        for cat in getTop(catSales, noCats):
+            print(cat)
+    print('{}\n'.format(divider))
+
+''' takes in a filepointer as parameter
+prints summary of the purchases records'''
+def getSummary(fp):
+    citySales, catSales = {}, {}
+    catGet = catSales.get
+    fgetline = fp.readline
+    totalSale = 0
+    line = fgetline()
+    while line:
+        splitline = line.split("\t")
+        salesAmt = lineValidation(splitline)
+        if salesAmt == None: # skip line if validation failed
+            line = fgetline()
+            continue
+        totalSale += salesAmt # calculate total sales amount
+        city, cat = splitline[2], splitline[3]
+        try:
+            citySales[city] += salesAmt # calculate city sales amount
+        except KeyError:
+            citySales[city] = salesAmt # add city to dictionary
+        try:
+            catSales[cat] += salesAmt # calculate category sales amount
+        except KeyError:
+            catSales[cat] = salesAmt # add category to dictionary
+        line = fgetline()
+    printSummary(citySales, catSales, totalSale)
--- phase1_3177.py
+++ phase1_3177.py
@@ -1,132 +1,37 @@
 #!/usr/bin/python3
-import datetime, sys, os
-from shutil import rmtree
-from heapq import nlargest, nsmallest
+from sys import argv
+from datetime import datetime
+from multiprocessing import Process
+from functions_3177 import getSummary, writeRecords
 '''
 Author: David Zhu (P1703177)
 Class: DISM/FT/1A/21
-11s execution time
 '''
 
-'''reference: https://stackoverflow.com/questions/273192/how-can-i-create-a-directory-if-it-does-not-exist
-https://stackoverflow.com/questions/6996603/how-to-delete-a-file-or-folder
-Takes in citySales dictionary and original file pointer as parameters
-Writes indivial city purchase records out to disk'''
-def writeRecords(citySales, ogfp):
-    folder = 'reports'
-    if os.path.exists(folder):
-        rmtree(folder) # empty and remove directory if directory already exisits
-    os.makedirs(folder) # create reports directory
-    for city in citySales: # create file pointer for all cities
-        citySales[city] = open(folder + '/'+city+'.txt', 'w')
-    line = ogfp.readline()
-    while line:
-        tmp = line.split("\t")
-        if len(tmp) != 6: # skip line if it has invalid format
-            line = ogfp.readline()
-            continue
-        citySales.get(tmp[2]).write(line) # write line out to respective city file
-        line = ogfp.readline()
-    for fp in citySales.values(): # close file pointers
-        fp.close()
-
-'''reference: https://www.w3resource.com/python-exercises/dictionary/python-data-type-dictionary-exercise-22.php
-takes in a dictonary and n as parameter where n is number of top values
-returns top three keys with values from dict'''
-def getTop(dictionary, n):
-    topThree = nlargest(n, dictionary, key=dictionary.get) # get n keys with highest values
-    for i in range(n):
-        topThree[i] = '{:<23}{:23.2f}'.format(topThree[i], dictionary[topThree[i]])
-    return topThree
-
-''' takes in a dictonary and n as parameter where n is number of top values
-returns bottom three keys with values from dict '''
-def getBottom(dictionary, n):
-    btmThree = nsmallest(n, dictionary, key=dictionary.get) # get n keys with lowest values
-    btmThree.reverse()
-    for i in range(n):
-        btmThree[i] = '{:<23}{:23.2f}'.format(btmThree[i], dictionary[btmThree[i]])
-    return btmThree
-
-''' 
-takes in citySales, category sales and total sales amt as parameters
-prints out the sales summary for all cities and categories '''
-def printSummary(citySales, catSales, totalSale):
-    noCities = len(citySales)
-    noCats = len(catSales)
-    avgCitySale = totalSale / noCities
-    avgCatSale = totalSale / noCats
-    print("Total Sales of the year is {:.2f}\n".format(totalSale))
-    print("The Average Sales From {} Cities:\n{:46.2f}\n".format(noCities, avgCitySale))
-    if noCities > 3:
-        print("Top Three Cities\n{}".format('=' * 46))
-        for city in getTop(citySales, 3):
-            print(city)
-        print('=' * 46 + '\n')
-        print("Bottom Three Cities\n{}".format('=' * 46))
-        for city in getBottom(citySales, 3):
-            print(city)
-    else:
-        print("Sales Figures by Cities\n{}".format('=' * 46))
-        for city in getTop(citySales, noCities):
-            print(city)
-    print('=' * 46 + '\n')
-    print("The Average Sales From {} Item Categories:\n{:46.2f}\n".format(noCats, avgCatSale))
-    if noCats > 3:
-        print("Top Three Item Categories\n{}".format('=' * 46))
-        for cat in getTop(catSales, 3):
-            print(cat)
-        print('=' * 46 + '\n')
-        print("Bottom Three Item Categories\n{}".format('=' * 46))
-        for cat in getBottom(catSales, 3):
-            print(cat)
-    else:
-        print("Sales Figures by Item Categories\n{}".format('=' * 46))
-        for cat in getTop(catSales, noCats):
-            print(cat)
-    print('=' * 46 + '\n')
-
 def main():
-    if len(sys.argv) != 2:
-        print('Usage: {} <sales data file>'.format(sys.argv[0]))
+    if len(argv) != 2:
+        print('Usage: {} <sales data file>'.format(argv[0]))
         return
     try:
-        f = open(sys.argv[1], 'r')
+        fp1 = open(argv[1], 'r')
+        fp2 = open(argv[1], 'r')
     except FileNotFoundError:
         print("Invalid data file. Operation aborted.")
         return
     else:
-        print(datetime.datetime.now().strftime("%a %b %d %X"))
-        citySales = {}
-        catSales = {}
-        totalSale = 0
-        line = f.readline()
-        while line:
-            tmp = line.split("\t")
-            if len(tmp) != 6: # skip line if it has invalid format
-                line = f.readline()
-                continue
-            try:
-                salesAmt = float(tmp[4])
-            except ValueError: # skip line if it has invalid data
-                line = f.readline()
-                continue
-            totalSale += salesAmt # calculate total sales amount
-            if tmp[2] not in citySales:
-                citySales[tmp[2]] = salesAmt # add city to dictionary
-            else:
-                citySales[tmp[2]] += salesAmt # calculate city sales amount
-            if tmp[3] not in catSales:
-                catSales[tmp[3]] = salesAmt # add categories to dictionary
-            else:
-                catSales[tmp[3]] += salesAmt # calculate city sales amount
-            line = f.readline()
-
-        printSummary(citySales, catSales, totalSale)
-        f.seek(0, 0) # reset filepointer
-        writeRecords(citySales, f)
-        f.close()
-        print(datetime.datetime.now().strftime("%a %b %d %X"))
+        print(datetime.now().strftime("%a %b %d %X"))
+        p1 = Process(target=writeRecords, args=(fp1,))
+        p2 = Process(target=getSummary, args=(fp2,))
+        # Start processes
+        p1.start()
+        p2.start()
+        # Wait for processes to finish
+        p1.join()
+        p2.join()
+        # Close file pointers
+        fp1.close()
+        fp2.close()
+        print(datetime.now().strftime("%a %b %d %X"))
 
 if __name__ == '__main__':
     main()
